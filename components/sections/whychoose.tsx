b3695e6d9d58b1ba860f1c737ae86acc8d0f75a5
lyrarod <lyrarod@gmail.com>
2024-11-18 20:17:48 -0300

    adjust bg-color & font-size

--- components/sections/whychoose.tsx
+++ components/sections/whychoose.tsx
@@ -10,32 +10,16 @@ export const WhyChoose = () => {
         </h2>
 
         <div className="grid gap-8 mt-8 lg:grid-cols-3">
-          {[
-            {
-              icon: Sparkles,
-              title: "Premium Quality",
-              description:
-                "We use eco-friendly products and advanced cleaning techniques",
-            },
-            {
-              icon: Clock,
-              title: "Always On Time",
-              description: "Punctual service with respect for your schedule",
-            },
-            {
-              icon: Shield,
-              title: "Fully Insured",
-              description:
-                "Your property is protected with our comprehensive insurance",
-            },
-          ].map((feature, i) => {
+          {chooses.map((choose, i) => {
             return (
-              <Card key={i}>
+              <Card key={i} className="bg-white dark:bg-background">
                 <div className="flex flex-col gap-4 p-6">
-                  <feature.icon className="lg:size-8 text-primary" />
-                  <strong>{feature.title}</strong>
+                  <choose.icon className="lg:size-8 text-primary" />
+                  <strong className="text-sm lg:text-base">
+                    {choose.title}
+                  </strong>
                   <p className="text-sm lg:text-base text-muted-foreground">
-                    {feature.description}
+                    {choose.description}
                   </p>
                 </div>
               </Card>
@@ -46,3 +30,22 @@ export const WhyChoose = () => {
     </section>
   );
 };
+
+const chooses = [
+  {
+    icon: Sparkles,
+    title: "Premium Quality",
+    description:
+      "We use eco-friendly products and advanced cleaning techniques",
+  },
+  {
+    icon: Clock,
+    title: "Always On Time",
+    description: "Punctual service with respect for your schedule",
+  },
+  {
+    icon: Shield,
+    title: "Fully Insured",
+    description: "Your property is protected with our comprehensive insurance",
+  },
+];
